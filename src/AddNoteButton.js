24c4f8ff5e4e8842aeaeab8326c70b6446f31d1e
Kev Comedia <kevcomedia@gmail.com>
2019-06-10 20:53:04 +0800

    Write new note button styles

--- src/AddNoteButton.js
+++ src/AddNoteButton.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './AddNoteButton.css';
 
 function AddNoteButton(props) {
   const handleClick = (e) => {
     props.dispatch({ type: 'add' });
   };
   return (
-    <button type="button" onClick={handleClick}>
+    <button className="add-note-button" type="button" onClick={handleClick}>
       <FontAwesomeIcon icon="plus" fixedWidth />
       New Note
     </button>
