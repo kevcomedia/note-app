235db0123c3ecedaf57342b648c2b6738ea2d25f
Kev Comedia <kevcomedia@gmail.com>
2019-06-10 18:52:25 +0800

    Focus on editor on note select

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import NoteList from './NoteList';
 import Editor from './Editor';
 import './App.css';
 
 function App(props) {
+  const editorRef = useRef();
+
   const [selectedNoteId, setSelectedNoteId] = useState(null);
   const selectedNote = props.notes.find((note) => note.id === selectedNoteId);
+
+  useEffect(
+    function focusEditor() {
+      if (editorRef.current) {
+        editorRef.current.focus();
+        editorRef.current.selectionEnd = 0;
+      }
+    },
+    [selectedNoteId],
+  );
 
   return (
     <div className="App">
@@ -15,7 +27,11 @@ function App(props) {
         selectedNoteId={selectedNoteId}
         onNoteSelect={setSelectedNoteId}
       />
-      <Editor className="App__main" selectedNote={selectedNote} />
+      <Editor
+        className="App__main"
+        selectedNote={selectedNote}
+        ref={editorRef}
+      />
     </div>
   );
 }
--- src/Editor.js
+++ src/Editor.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import './Editor.css';
 
-function Editor({ className, selectedNote }) {
-  return (
-    <textarea
-      className={`editor ${className}`}
-      value={selectedNote ? selectedNote.text : ''}
-    />
-  );
-}
+// forwardRef used so the textarea can be focused from outside (like when a note
+// is selected).
+const Editor = forwardRef(({ className, selectedNote }, ref) => (
+  <textarea
+    className={`editor ${className}`}
+    value={selectedNote ? selectedNote.text : ''}
+    ref={ref}
+  />
+));
 
 export default Editor;
