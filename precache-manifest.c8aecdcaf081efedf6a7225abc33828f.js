a0c55fb1368e84a3fb3840fafc8a54a76e0bd424
Kev Comedia <kevcomedia@gmail.com>
2019-06-13 17:28:14 +0800

    Updates

---
+++ precache-manifest.c8aecdcaf081efedf6a7225abc33828f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c05aad5f5555c09e357b6b940205ab00",
+    "url": "/note-app/index.html"
+  },
+  {
+    "revision": "0ff3316e50e0a751d3c7",
+    "url": "/note-app/static/css/main.6b79f9c6.chunk.css"
+  },
+  {
+    "revision": "a7ddba5eec4d9256d78e",
+    "url": "/note-app/static/js/2.e91f8f5a.chunk.js"
+  },
+  {
+    "revision": "0ff3316e50e0a751d3c7",
+    "url": "/note-app/static/js/main.bccfcae7.chunk.js"
+  },
+  {
+    "revision": "47b98c4c94c3392872d3",
+    "url": "/note-app/static/js/runtime~main.a60a26f2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a99a9a270c903b08a6526c12e8321a0.js"
+  "/note-app/precache-manifest.c8aecdcaf081efedf6a7225abc33828f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/note-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
